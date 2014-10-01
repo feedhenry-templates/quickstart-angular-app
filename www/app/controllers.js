50a0a4848b55b240daf0cee11d16e6389509554a
Niall Donnelly <niall.donnelly@feedhenry.com>
2014-10-01 12:14:08 +0100

    Added cloud functionality to angular app

--- www/app/controllers.js
+++ www/app/controllers.js
@@ -4,5 +4,36 @@ var myApp = angular.module('myApp.controllers', ['fhcloud']);
 
 myApp.controller('MainCtrl', function($scope, fhcloud) {
     console.log('MainCtrl', $scope);
-
+    // add function to pass userInput to cloud via
+    // $fh.cloud call to controller scope
+    $scope.getNumberOfCharacters = function(userInput) {
+      console.log("getNumberOfCharacters", userInput);
+      var reqJson = {
+        "head": {},
+        "payload": {
+          "userInput": userInput
+        }
+      };
+      // check if userInput is defined
+      if (reqJson.payload.userInput) {
+        // if defined, pass userInput and callback
+        // fn to fhcloud service
+        fhcloud.cloud('example', reqJson, success, error);
+      }
+    };
+    // success callback function
+    var success = function(res) {
+      console.log("fhcloud > exampleAction > success", res);
+      // if successful redirect to home
+      if (res.strLength != null || undefined) {
+        //  if success do something
+        alert("You entered " + res.strLength + " characters!");
+      } else {
+        alert("Successcb > Something is wrong!!");
+      }
+    };
+    // error callback function
+    var error = function(msg, err) {
+      alert("fhcloud > example > failed " + JSON.stringify(err));
+    };
 });
